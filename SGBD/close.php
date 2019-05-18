7b759cb5103e48815095c5c6c94e9a036346ffe2
paulsem <32594551+paulsem@users.noreply.github.com>
2019-05-18 20:42:04 +0300

    Create close.php

---
+++ SGBD/close.php
@@ -0,0 +1,13 @@
+<?php
+include 'Connection.php';  
+
+$update = "update student SET prezence_status='0' WHERE group_student='e1'";
+$stmt = oci_parse($conn, $update);
+$result = oci_execute($stmt, OCI_COMMIT_ON_SUCCESS );
+//oci_commit($result);
+//var_dump($stmt);
+if (!$result) {
+  echo oci_error();   
+}
+header("Location: /SGBD/Prezente_teacher.php");
+?>
