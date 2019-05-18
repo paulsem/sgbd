cb7cd712f3b620f9ac77ecb10cbf9933b78ac497
paulsem <32594551+paulsem@users.noreply.github.com>
2019-05-18 20:42:29 +0300

    Update Prezente_teacher.php

--- SGBD/Prezente_teacher.php
+++ SGBD/Prezente_teacher.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
-
+<?php
+include 'Connection.php';    
+?>
 <head>
     <link rel="stylesheet" type="text/css" href="style1.css">
     <link rel="stylesheet" type="text/css" href="Grid.css">
@@ -10,6 +12,8 @@ function myFunction() {
   document.getElementById("demo").innerHTML = Math.random().toString(36).substring(2, 15);
 }
 </script>
+    
+   
 </head>
 
 <body>
@@ -21,13 +25,26 @@ function myFunction() {
                     <?php
                         session_start();
                         echo "<h1>Welcome ".$_SESSION['t_username']."</h1>";
+                    echo "<h1>Welcome ".$_SESSION['t_grupa']."</h1>";
+                    $x=$_SESSION['t_grupa'];
                     ?>
-
-                    <button id="demo" type="button" name="button" onclick="myFunction()">Gen Code</button>
-
+                     <form action="open.php" method="post">
+                        
+                         <input name="submit" type="submit" value="open">
+                    </form>
+                    <br>
+                    <form action="close.php" method="post">
+                        
+                         <input name="submit" type="submit" value="close">
+                    </form>
+                   <br>
+                    
+                    
                     <?php
-                        echo "<br><br><a href='Logout_teacher.php'><input type=button value=Logout></a>";   
+                        echo "<br><a href='Logout_teacher.php'><input type=button value=Logout></a>";  
+                        
                     ?>
+                    
                     <br>
                     <br>
                 </div>
@@ -35,11 +52,43 @@ function myFunction() {
                 <div class="box">
                     <div class="box3">
                         <h2>Max prez</h2>
-                        <h2>5/10 - username</h2>
+                        
+                        <?php
+                        
+                        $sql3 = "select MAX( p.prezence )
+from prezence p inner join student s on p.username = s.username_student where s.group_student='$x'";
+                        
+                        $result3 = oci_parse($conn,$sql3);            
+                        oci_execute($result3);
+                        if($row3=oci_fetch_assoc($result3)){
+                        
+                        echo '<h2>'.$row3['MAX(P.PREZENCE)'].'</h2>';
+                        $aux=$row3['MAX(P.PREZENCE)'];
+                        }
+                       
+                        
+                        ?>
+                        
+                        <!--<h2>5/10 - username</h2>-->
                     </div>
                     <div class="box3">
                         <h2>Min prez</h2>
-                        <h2>1/10 - username</h2>
+                        
+                        <?php
+                        
+                        $sql2 = "select MIN( p.prezence )
+from prezence p inner join student s on p.username = s.username_student where s.group_student='$x'";
+                        
+                        $result2 = oci_parse($conn,$sql2);            
+                        oci_execute($result2);
+                        
+                        if($row2=oci_fetch_assoc($result2)){
+                        
+                        echo '<h2>'.$row2['MIN(P.PREZENCE)'].'</h2>';
+                        }
+                        oci_commit($conn)
+                        ?>
+                        <!--<h2>1/10 - username</h2>-->
                     </div>
                     <div class="box3">
                         <h2>Prez la ora</h2>
@@ -54,6 +103,17 @@ function myFunction() {
             </div>
 
 <br>
+            <?php
+           
+            $sql = "SELECT * FROM student WHERE group_student = '$x'";
+            $result = oci_parse($conn,$sql);            
+            oci_execute($result);
+            if(!$result){
+             echo 'grupa gresita';
+             exit();
+            }
+                
+            ?>
             <div class="box2">
 
                 <div class="row">
@@ -61,69 +121,31 @@ function myFunction() {
                     <br>
                     <br>
                     <table>
-                        <tr>
+                    <tr>
                             <th>ID</th>
                             <th>Username</th>
                             <th>prezente</th>
                             <th>procentaj</th>
                         </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jill</td>
-                            <td>2/10</td>
-                            <td>50</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jill</td>
-                            <td>3/10</td>
-                            <td>50</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jill</td>
-                            <td>3/10</td>
-                            <td>50</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jill</td>
-                            <td>3/10</td>
-                            <td>50</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jill</td>
-                            <td>3/10</td>
-                            <td>50</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jill</td>
-                            <td>3/10</td>
-                            <td>50</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jill</td>
-                            <td>3/10</td>
-                            <td>50</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Jill</td>
-                            <td>3/10</td>
-                            <td>50</td>
-                        </tr>
-
-                        <tr>
-                            <td>2</td>
-                            <td>Jill</td>
-                            <td>3/10</td>
-                            <td>50</td>
-                        </tr>
-
-
+                    <?php
+                    while ($row = oci_fetch_assoc($result)) { 
+                        echo "<tr>\n"; 
+                        echo "<td>". $row["ID_STUDENT"] . "</td>\n"; 
+                        echo "<td>". $row["USERNAME_STUDENT"] . "</td>\n"; 
+                        $x=$row["USERNAME_STUDENT"];
+                        $sql1="SELECT * FROM prezence WHERE username = '$x'";
+                        
+                        $result1 = oci_parse($conn,$sql1);            
+                        oci_execute($result1);
+                        if($row1 = oci_fetch_assoc($result1)){
+                        
+                            echo "<td>". $row1["PREZENCE"] . "</td>\n";
+                        }
+                        echo "<td>". $row["PREZENCE_STATUS"] . "</td>\n"; 
+                        echo "</tr>\n"; 
+}     
+                        oci_commit($conn)
+                    ?>
 
 
                     </table>
@@ -132,6 +154,7 @@ function myFunction() {
                 </div>
 
             </div>
+          
             <br>
             <br>
 
