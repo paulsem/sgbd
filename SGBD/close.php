a5499e44730937853be6620b59bbeb318b75eba8
paulsem <32594551+paulsem@users.noreply.github.com>
2019-05-19 23:08:19 +0300

    Update close.php

--- SGBD/close.php
+++ SGBD/close.php
@@ -1,13 +1,17 @@
 <?php
 include 'Connection.php';  
-
-$update = "update student SET prezence_status='0' WHERE group_student='e1'";
-$stmt = oci_parse($conn, $update);
-$result = oci_execute($stmt, OCI_COMMIT_ON_SUCCESS );
-//oci_commit($result);
-//var_dump($stmt);
-if (!$result) {
-  echo oci_error();   
+if(isset($_POST['submit'])){
+   // var_dump($_POST['hidden']);
+    $x=$_POST['hidden'];
+    echo $x;
+    $update = "update student SET prezence_status='0' WHERE lower(group_student)='$x'";
+    $stmt = oci_parse($conn, $update);
+    $result = oci_execute($stmt, OCI_COMMIT_ON_SUCCESS );
+    //oci_commit($result);
+    //var_dump($stmt);
+    if (!$result) {
+      echo oci_error();   
+    }
 }
 header("Location: /SGBD/Prezente_teacher.php");
 ?>
