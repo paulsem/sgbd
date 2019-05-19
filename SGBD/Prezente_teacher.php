3605b4b3903a7736a7326c399595e6818ed8ec25
paulsem <32594551+paulsem@users.noreply.github.com>
2019-05-19 12:40:33 +0300

    Update Prezente_teacher.php

--- SGBD/Prezente_teacher.php
+++ SGBD/Prezente_teacher.php
@@ -51,19 +51,30 @@ function myFunction() {
               
                 <div class="box">
                     <div class="box3">
-                        <h2>Max prez</h2>
+                        <h2>Max prez
                         
                         <?php
                         
                         $sql3 = "select MAX( p.prezence )
-from prezence p inner join student s on p.username = s.username_student where s.group_student='$x'";
+from prezence p inner join student s on p.username = s.username_student where lower(s.group_student)='$x'";
                         
                         $result3 = oci_parse($conn,$sql3);            
                         oci_execute($result3);
                         if($row3=oci_fetch_assoc($result3)){
                         
-                        echo '<h2>'.$row3['MAX(P.PREZENCE)'].'</h2>';
+                        echo $row3['MAX(P.PREZENCE)'];
                         $aux=$row3['MAX(P.PREZENCE)'];
+                        }?>
+                        </h2><?php
+                        
+                        $sql4 = "select username from prezence where prezence='$aux'";
+                        
+                        $result4 = oci_parse($conn,$sql4);            
+                        oci_execute($result4);
+                        if($row4=oci_fetch_assoc($result4)){
+                        
+                        echo '<h2>'.$row4['USERNAME'].'</h2>';
+                        
                         }
                        
                         
@@ -72,19 +83,31 @@ from prezence p inner join student s on p.username = s.username_student where s.
                         <!--<h2>5/10 - username</h2>-->
                     </div>
                     <div class="box3">
-                        <h2>Min prez</h2>
+                        <h2>Min prez
                         
                         <?php
                         
                         $sql2 = "select MIN( p.prezence )
-from prezence p inner join student s on p.username = s.username_student where s.group_student='$x'";
+from prezence p inner join student s on p.username = s.username_student where lower(s.group_student)='$x'";
                         
                         $result2 = oci_parse($conn,$sql2);            
                         oci_execute($result2);
                         
                         if($row2=oci_fetch_assoc($result2)){
                         
-                        echo '<h2>'.$row2['MIN(P.PREZENCE)'].'</h2>';
+                        echo $row2['MIN(P.PREZENCE)'];
+                        $aux=$row2['MIN(P.PREZENCE)'];
+                        }
+                            ?></h2><?php
+                        
+                        $sql4 = "select username from prezence where prezence='$aux'";
+                        
+                        $result4 = oci_parse($conn,$sql4);            
+                        oci_execute($result4);
+                        if($row4=oci_fetch_assoc($result4)){
+                        
+                        echo '<h2>'.$row4['USERNAME'].'</h2>';
+                        
                         }
                         oci_commit($conn);
                         ?>
@@ -94,7 +117,8 @@ from prezence p inner join student s on p.username = s.username_student where s.
                         <h2>Prez la ora</h2>
                         
                         <?php 
-                        $sql2 = "select count(*) from prezence where data >= (sysdate-2/24)";
+                        $sql2 = "select count(*)
+from prezence p inner join student s on p.username = s.username_student where lower(s.group_student)='$x'";
                         
                         $result2 = oci_parse($conn,$sql2);            
                         oci_execute($result2);
@@ -117,7 +141,7 @@ from prezence p inner join student s on p.username = s.username_student where s.
 <br>
             <?php
            
-            $sql = "SELECT * FROM student WHERE group_student = '$x'";
+            $sql = "SELECT * FROM student WHERE lower(group_student) = '$x'";
             $result = oci_parse($conn,$sql);            
             oci_execute($result);
             if(!$result){
@@ -138,7 +162,8 @@ from prezence p inner join student s on p.username = s.username_student where s.
                             <th>Username</th>
                             <th>prezente</th>
                             <th>points</th>
-                            <th>add points</th>
+                            <th>add/remove points</th>
+                        <th>remove prezence</th>
                         </tr>
                     <?php
                     while ($row = oci_fetch_assoc($result)) { 
@@ -156,15 +181,29 @@ from prezence p inner join student s on p.username = s.username_student where s.
                             echo "<td>". $row1["POINTS"] . "</td>\n";
                         }
                         /*echo "<td>". $row["PREZENCE_STATUS"] . "</td>\n"; */
-                        echo "</tr>\n"; ?>
+                       ?>
                         <td>
                             <form method="post" action="add_points.php"> 
                                 <input type="hidden" name="hidden"  value="<?php echo $row['USERNAME_STUDENT'];?>">
                                 
                                 <input type="submit" name="submit" value="+">
                             </form>
+                            <form method="post" action="remove_points.php"> 
+                                <input type="hidden" name="hidden"  value="<?php echo $row['USERNAME_STUDENT'];?>">
+                                
+                                <input type="submit" name="submit" value="-">
+                            </form>
+                        </td>
+                        <td>
+                            <form method="post" action="remove_prezence.php"> 
+                                <input type="hidden" name="hidden"  value="<?php echo $row['USERNAME_STUDENT'];?>">
+                                
+                                <input type="submit" name="submit" value="-">
+                            </form>
+                        
                         </td>
                 <?php
+                          echo "</tr>\n";
 }     
                         oci_commit($conn)
                     ?>
